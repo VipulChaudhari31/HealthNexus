9ccff9c9ff216edbadaa5df23ec96c03a5087964
Harshwardhan Deshmukh <harshwardhanrdd@gmail.com>
2023-11-12 00:56:48 +0530

    temp changes

--- healthNexus/portal/urls.py
+++ healthNexus/portal/urls.py
@@ -2,6 +2,5 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('portal.urls')),
+    path('',views.home,name="home"),
 ]
