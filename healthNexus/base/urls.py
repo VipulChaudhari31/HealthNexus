349ace95e09beeb1b278b6d18e91eeeec0a56919
aditya-choudhary599 <choudharyap21.comp@coep.ac.in>
2023-11-12 09:38:33 +0530

    Corrected models and added image field

--- healthNexus/base/urls.py
+++ healthNexus/base/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import *
 
 urlpatterns = [
     path('',home_page_view,name='home_page'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
